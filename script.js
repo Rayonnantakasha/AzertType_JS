72440c4c74112ff1b5b54d31e771bd702ef0210a
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-10-28 14:42:05 +0630

    Feat : add execice words and sentenses whith a boucle while and for and the score

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 const litMots = ["Cachalot", "Pétunia", "Serviette"]
+const listPhrases = ["La vie", "l/'univers et le reste", "Merci pour le poisson"]
 let score = 0
+let choix = prompt("Voulez-vous taper des mots ou des phrases ?")
 
-for (let i = 0; i<litMots.length; i++) {
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Voulez-vous taper des mots ou des phrases ?")
+}
+
+if (choix === "mots") {
+    for (let i = 0; i < litMots.length; i++) {
+        let motUtilisateur = prompt("Entrer votre mot " + litMots[i])
+        if (motUtilisateur === litMots[i]) {
+            score++
+        }
+    }
+    alert("Votre score est de " + score + " sur " + litMots.length)
+}
+
+if (choix === "phrases") {
+    for (let i = 0; i < listPhrases.length; i++) {
+        let phraseUtilisateur = prompt("Entrer votre phrase " + listPhrases[i])
+        if (phraseUtilisateur === listPhrases[i]) {
+            score++
+        }
+    }
+    alert("Votre score est de " + score + " sur " + listPhrases.length)
+}
+
+/*
+for (let i = 0; i < litMots.length; i++) {
     let motUtilisateur = prompt("Entrer votre mot " + litMots[i])
     if (motUtilisateur === litMots[i]) {
         score++
     }
 }
 alert("Votre score est de " + score)
+*/
