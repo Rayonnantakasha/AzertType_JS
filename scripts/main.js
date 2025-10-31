d85281e3899e2318f6b317a11dbef63e8f9ab565
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-10-31 11:14:18 +0630

    Recuperation des elements HTML dans le DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 lancerJeu()
+
+//selection des élements avec le DOM
+
+const btnValiderMot = document.getElementById('btnValiderMot')
+const inputEcriture = document.getElementById('inputEcriture')
+const zoneProposition = document.querySelector('.zoneProposition')
+const spanScore = document.querySelector('.zoneScore span')
+const btnOptionSource = document.querySelectorAll('.optionSource input')
