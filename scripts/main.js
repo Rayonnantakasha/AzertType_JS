40118b4d410df1649474b949f0df1582ff188093
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-11-01 14:13:56 +0630

    set modicfications

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,3 @@ lancerJeu()
 
 //selection des élements avec le DOM
 
-const btnValiderMot = document.getElementById('btnValiderMot')
-const inputEcriture = document.getElementById('inputEcriture')
-const zoneProposition = document.querySelector('.zoneProposition')
-const spanScore = document.querySelector('.zoneScore span')
-const btnOptionSource = document.querySelectorAll('.optionSource input')
